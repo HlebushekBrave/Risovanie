420530df6ea154589154418051b9c3dcdcb5d8ef
adristifauzi <73088851+adristifauzi@users.noreply.github.com>
2021-03-26 18:18:53 +0700

    Update 🌝.py

--- 🌝.py
+++ 🌝.py
@@ -3,6 +3,7 @@
 #Adristi Fauzi
 #Alexandra Raspopova
 import turtle
+import math
 
 #для 2 цветов и количества шестиугольников (рабочее):
 def get_color_choice(n):
@@ -38,6 +39,63 @@ def get_num_hexagons(count_hexagon):
             i = 0
             count_hexagon = input('Оно должно быть от 4 до 20. Пожалуйста, повторите попытку: ')
             continue
+
+def hexagon(x, y, side_len, color):
+    import turtle
+    # creating turtle pen
+    t = turtle.Turtle()
+    import math
+    # Set coordinate
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    # set the fillcolor
+    t.fillcolor(color)
+    # start the filling color
+    t.begin_fill()
+    t.right(30)
+    t.forward(side_len)
+    t.left(60)
+    t.forward(side_len)
+    t.left(60)
+    t.forward(side_len)
+    t.left(60)
+    t.forward(side_len)
+    t.left(60)
+    t.forward(side_len)
+    t.left(60)
+    t.forward(side_len)
+
+    t.hideturtle()
+    t.end_fill()
+    return
+
+def draw_hexagon(side_len, x, y, color, segment):
+    import math
+    import turtle as t
+    k = 0
+    rad=math.radians(30)
+    d = 2*((math.cos(rad))*side_len)
+    c = (math.sin(rad)*side_len)
+    col1=color[0]
+    col2=color[1]
+
+    for i in range(0, segment):
+        y1 = y + i*(side_len + c)
+        if i%2 == 0: #from base
+            for j in range(0, segment):
+                x1 = x + (j * d)
+                #y1 = y
+                hexagon(x1, y1, side_len, col1)
+        else:
+            for j in range(0, segment):
+                x_else = x + (d / 2)
+                x1 = x_else + j * d
+                #y1 = y + side_len + c
+                hexagon(x1, y1, side_len, col2)
+    t.done()
+    return
+
 first_color = input('Пожалуйста, введите цвет: ')
 n=first_color
 a = 'красный'
@@ -95,3 +153,15 @@ while o!=1:
         count_hexagon = input('Оно должно быть от 4 до 20. Пожалуйста, повторите попытку: ')
 counts=int(get_num_hexagons(count_hexagon))
 print(col1, col2)
+
+color = [col1, col2]
+#print(color)
+
+x = -200 #first point coordinate
+y = -200 #first point coordinate
+
+segment = counts
+rad=math.radians(30)
+d1 = 500 / segment
+side_len = (d1/2)-math.cos(rad)
+draw_hexagon(side_len, x, y, color, counts)
